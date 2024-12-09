32e085c0b5b750b8cc6e78ed2acd50ef8eaebd42
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2024-12-09 18:22:21 +0900

    improve day 09 part 01
    from 5 minutes to 5 seconds

--- app/Day09.hs
+++ app/Day09.hs
@@ -5,9 +5,7 @@ import Text.Megaparsec.Char (digitChar)
 
 import Common
 import Data.Char (digitToInt)
-import Data.Function (on)
-import Data.List (findIndex, groupBy)
-import Data.Maybe (isJust, isNothing, fromJust)
+import Data.Maybe (isJust, fromJust)
 
 -- File Id  | EmptySpace
 type DiskItem = Maybe Int
@@ -23,16 +21,20 @@ parser = (toDiskItems True 0) <$> digits
             | isFile = (take x $ repeat (Just nextFileId)) ++ (toDiskItems False (nextFileId + 1) xs)
             | otherwise = (take x $ repeat Nothing) ++ (toDiskItems True nextFileId xs)
 
+-- compact' constructs the compressed filesystem reversed to save time (because (:) is O(1) whereas (++) is O(n))
+-- so we reverse the reversed result in compact  
 compact :: [DiskItem] -> [DiskItem]
-compact filesystem
-  | noGapPresent = groupBy ((&&) `on` isJust) filesystem !! 0
-  | otherwise = Common.updateListAt newFilesystem lastFile firstGapIndex  |> compact
-  where noGapPresent = groupBy ((&&) `on` isJust) filesystem |> length |> (<=2)
-        firstGapIndex = findIndex isNothing filesystem |> fromJust
-        reversedFilesystem = reverse filesystem
-        lastFileIndex = findIndex isJust reversedFilesystem |> fromJust
-        lastFile = reversedFilesystem !! lastFileIndex
-        newFilesystem = Common.deleteListAt reversedFilesystem lastFileIndex |> reverse
+compact filesystem = compact' filesystem (reverse filesystem) [] |> reverse
+  where compact' :: [DiskItem] -> [DiskItem] -> [DiskItem] -> [DiskItem]
+        compact' filesystem' reversedFs finalFs
+          | isDone = finalFs
+          | otherwise = case (filesystem', reversedFs) of
+              ((Just x:xs), (reversedFs@(Just _:_))) -> compact' xs reversedFs (Just x:finalFs)
+              ((Nothing:xs), (Just y:ys)) -> compact' xs ys (Just y:finalFs)
+              ((Just x:xs), (Nothing:ys)) -> compact' xs ys (Just x:finalFs)
+              (filesystem@(Nothing:_), (Nothing:ys)) -> compact' filesystem ys finalFs
+              _ -> error "unreachable"
+          where isDone = length finalFs == length (filter isJust filesystem)
 
 partOne :: [DiskItem] -> Int
 partOne filesystem =
